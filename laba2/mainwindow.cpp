6a7f255f016a43c23e853da935253ae8d028cacc
Maria <mashashakuro25@gmail.com>
2024-07-03 00:29:51 +0300

    new version

--- laba2/mainwindow.cpp
+++ laba2/mainwindow.cpp
@@ -2,13 +2,30 @@
 #include "ui_mainwindow.h"
 #include<fstream>
 #include<vector>
+#include<string>
 #include<algorithm>
 #include<QTableWidgetItem>
+#include<QStringListModel>
+#include<QObject>
+#include<QPixmap>
+#include<QDebug>
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->tableWidget->setContextMenuPolicy(Qt::CustomContextMenu);
+    connect(ui->tableWidget, &QTableView::customContextMenuRequested, [&](const QPoint& pos) {
+        static QMenu* menu = nullptr;
+
+        if (!menu) {
+            menu = new QMenu();
+            menu->addAction(new QAction("1", this));
+            menu->addAction(new QAction("2", this));
+            menu->addAction(new QAction("3", this));
+        }
+        menu->popup( mapToGlobal(pos) );
+    });
 }
 
 MainWindow::~MainWindow()
@@ -35,7 +52,7 @@ void MainWindow::ReadFile()
                 qDebug() << "Не удалось открыть файл для чтения";
                 return;
             }
-
+            QStandardItemModel* model = new QStandardItemModel(this);
             QTextStream in(&file);
             ui->tableWidget->clearContents();
             ui->tableWidget->setRowCount(0);
@@ -56,9 +73,11 @@ void MainWindow::ReadFile()
                 }
 
                 ++row;
+                model->appendRow(new QStandardItem(line));
                 ui->listWidget->addItem(line);
             }
-            file.close();
+            this->ui->listView->setModel(model);
+          file.close();
         }
     }
     else{
@@ -70,7 +89,7 @@ void MainWindow::ReadFile()
                 qDebug() << "Не удалось открыть файл для чтения";
                 return;
             }
-
+            QStandardItemModel* model = new QStandardItemModel(this);
             QTextStream in(&file);
             ui->tableWidget->clearContents();
             ui->tableWidget->setRowCount(0);
@@ -91,10 +110,11 @@ void MainWindow::ReadFile()
                     // Записываем данные в ячейку таблицы
                     ui->tableWidget->setItem(row,col, new QTableWidgetItem(parts.at(col)));
                 }
-
+                model->appendRow(new QStandardItem(line));
                 ++row;
                 ui->listWidget->addItem(line);
             }
+            this->ui->listView->setModel(model);
             file.close();
         }
     }
@@ -211,39 +231,141 @@ void MainWindow::on_pushButton_3_clicked()
 
 }
 
+void MainWindow::binaryInsertionSort() {
+      QString file_name = this->current_file;
+    QFile file(file_name);
+    if (!file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+        qDebug() << "Не удалось открыть файл для чтения";
+        return;
+    }
+    QStandardItemModel* model = new QStandardItemModel(this);
+    QTextStream in(&file);
+    while (!in.atEnd()) {
+        //this->current_num++;
+        QString line = in.readLine();
+        QStringList parts = line.split(" "); // Предполагаем, что данные разделены пробелом
+        int columnCount = parts.size();
+        int row=0;
+        // Добавляем новую строку в таблицу
+        ui->tableWidget->insertRow(row);
 
-/*void MainWindow::binarySort(){
-    // Получение указателя на QTableWidget (this->ui->tableWidget)
-    QTableWidget* tableWidget = this->ui->tableWidget;
+       for (int col = 0; col < columnCount; ++col) {
+            // Записываем данные в ячейку таблицы
+          ui->tableWidget->setItem(row,col, new QTableWidgetItem(parts.at(col)));
+        }
 
-    // Получение количества строк в таблице
-    int rowCount = tableWidget->rowCount();
+        ++row;
+}
 
-    // Создание вектора для хранения значений первого столбца
-    std::vector<QTableWidgetItem*> firstColumnValues;
-    for (int row = 0; row < rowCount; ++row)
-    {
-        QTableWidgetItem* item = tableWidget->item(row, 0);
-        if (item)
-        {
-            firstColumnValues.push_back(item);
+    QTableWidget *tableWidget_ = this->ui->tableWidget;
+   int rowCount = tableWidget_->rowCount();
+
+    // Сортировка по первому столбцу
+    for (int i = 1; i < rowCount; ++i) {
+        QTableWidgetItem *key = tableWidget_->item(i, 0);
+
+        // Бинарный поиск
+        int left = 0;
+        int right = i - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            QTableWidgetItem *midItem = tableWidget_->item(mid, 0);
+            if (midItem->text().compare(key->text()) < 0) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        // Перемещение элементов в первом столбце
+        for (int j = i; j > right + 1; --j) {
+            tableWidget_->setItem(j, 0, tableWidget_->item(j - 1, 0));
+        }
+
+        // Вставка элемента в правильное положение
+        tableWidget_->setItem(right + 1, 0, key);
+
+        // Перемещение элементов в остальных столбцах (если нужно)
+        for (int col = 1; col < tableWidget_->columnCount(); ++col) {
+            // Сохранение данных из текущей строки
+            QTableWidgetItem *tempItem = tableWidget_->item(i, col);
+            // Перемещение данных из строки right + 1 в строку i
+            tableWidget_->setItem(i, col, tableWidget_->item(right + 1, col));
+            // Вставка сохраненных данных в строку right + 1
+            tableWidget_->setItem(right + 1, col, tempItem);
         }
     }
-    // Сортировка вектора с помощью бинарного поиска
-    std::sort(firstColumnValues.begin(), firstColumnValues.end(),[](QTableWidgetItem*& a, QTableWidgetItem*& b){
-        return a->text().toDouble() < b->text().toDouble();
-    });
 
-    // Обновление QTableWidget с отсортированными значениями
-    for (int row = 0; row < rowCount; ++row)
-    {
-        this->ui->tableWidget->setItem(row, 0, firstColumnValues[row]);
-    }
+}
 
-}*/
 void MainWindow::on_pushButton_2_clicked()
 {
-   // this->ui->tableWidget->binarySort();
+    this->binaryInsertionSort();
     this->ui->listWidget->sortItems();
+}
+
+
+void MainWindow::on_pushButton_4_clicked()
+{
+    QString file_name = this->current_file;
+
+        QFile file(file_name);
+        if (!file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+            qDebug() << "Не удалось открыть файл для чтения";
+            return;
+        }
+        QStandardItemModel* model = new QStandardItemModel(this);
+      QTextStream in(&file);
+         while (!in.atEnd()) {
+    QString line = in.readLine();
+
+     model->appendRow(new QStandardItem(line));
+     model->sort(0, Qt::DescendingOrder);
+         }
+      this->ui->listView->setModel(model);
+         file.close();
+
+}
+
+void MainWindow::ReadFile1()
+{
+
+    QFile file(current_file);
+    if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
+    {
+
+        qDebug() << "Error opening file:" << file.errorString();
+        return;
+    }
+
+    QTextStream in(&file);
+    while (!in.atEnd())
+    {
+        QString line = in.readLine();
+        qDebug() << line;
+    }
+
+    file.close();
+}
+void MainWindow::on_pushButton_5_clicked()
+{
+    QString file_name = QFileDialog::getOpenFileName(0,"Open dialog","","*.jpg");
+    this->current_file = file_name;
+    this->ReadFile1();
+
+    QPixmap pixmap1(":/img/img/mashina-1193x671px.jpg");
+    QPixmap pixmap2(":/img/img/mashina-1200x675px.jpg");
+    QPixmap pixmap3(":/img/img/mashina-1440x900px.jpg");
+
+    // Изменяем размеры изображений
+    int labelWidth = ui->label1->width();
+    int labelHeight = ui->label1->height();
+    pixmap1 = pixmap1.scaled(labelWidth, labelHeight, Qt::KeepAspectRatio);
+    pixmap2 = pixmap2.scaled(labelWidth, labelHeight, Qt::KeepAspectRatio);
+    pixmap3 = pixmap3.scaled(labelWidth, labelHeight, Qt::KeepAspectRatio);
+
+    ui->label1->setPixmap(pixmap1);
+    ui->label2->setPixmap(pixmap2);
+    ui->label3->setPixmap(pixmap3);
 }
 
